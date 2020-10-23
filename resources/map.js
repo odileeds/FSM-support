b404879a06f369b0ac4bbeac68b354f454fcfc93
slowe <gravatar@strudel.org.uk>
2020-10-23 12:47:24 +0100

    Debug

--- resources/map.js
+++ resources/map.js
@@ -130,7 +130,7 @@
 
 		this.get = function(){
 			var url = 'https://docs.google.com/spreadsheets/d/'+this.sheetid+'/gviz/tq?tqx=out:csv&sheet=details';
-			//url = "data.csv";
+			if(location.href.indexOf('file')==0) url = "data.csv";
 			console.info('Getting '+url);
 			ODI.ajax(url,{
 				"this":this,
@@ -149,11 +149,11 @@
 			var ocd,parea,district,sector;
 			pcd.replace(/^([^\s]+) ([0-9A-Z])/,function(m,p1,p2){ ocd = p1; sector = p2; return ""; });
 			ocd.replace(/^([A-Z]{1,2})([0-9]+|[0-9][A-Z])$/,function(m,p1,p2){ parea = p1; district = p2; return ""; });
-			var path = parea+'/'+district+'/'+sector+'.csv';
+			var path = parea+'/'+district+'/'+sector;
 			console.log('getPostcode',pcd,path,this.postcodes.loading[path]);
 			if(!this.postcodes.loading[path]){
 				this.postcodes.loading[path] = true;
-				ODI.ajax('postcodes/'+path,{
+				ODI.ajax('postcodes/'+path+'.csv',{
 					'dataType':'text/csv',
 					'this': this,
 					'path': path,
